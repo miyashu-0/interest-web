4e93dda1dc1b7427cd01360296fea8b89b95c824
miyazaki <miyazaki.kaishu@d-publishing.jp>
2023-11-12 00:03:59 +0900

    ログインビューファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/', to: 'users#index'
+  get '/', to: 'login#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
